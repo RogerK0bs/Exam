9b1381bee790aa89d9382b63b3e8373ee9d902eb
Leon9 <Leon9@K0BS>
2023-06-15 23:22:45 +0700

    Добавьте файлы проекта.

---
+++ Exam/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    enum Color
+    {
+        red = 0,
+        white= 1,
+        blue =2
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 0, 2, 1, 1, 0 };
+            int[] arr2 = new int[] { 2,0,1 };
+            Array.Sort(arr);
+            Array.Sort(arr2);
+            foreach (var item in arr)
+            {
+                Console.Write(item+"\t");
+            }
+            Console.WriteLine();
+            foreach (var item in arr2)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+            foreach(var item in arr)
+            {
+                switch (item)
+                {
+                   case 0:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(item + "\t");
+                        break;
+                   case 1:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(item + "\t");
+                        break;
+                   case 2:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(item + "\t");
+                        break;
+                }
+            }
+            Console.WriteLine();
+            foreach (var item in arr2)
+            {
+                switch (item)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(item + "\t");
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(item + "\t");
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(item + "\t");
+                        break;
+                }
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
